d927f1d02763d880c54854abac4b079c1a603528
uiuxkimabe <uiuxkimabe@gmail.com>
2023-08-10 23:44:18 +0700

    create svg animate

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-/*? no js js needed from me */
+// scroll nav
+const nav = document.getElementsByTagName("nav")[0];
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 1) {
+    nav.style.background = "#1d1d1d";
+  } else {
+    nav.style.background = "none";
+  }
+});
+
+// click menu hamBtn
+const hamBtn = document.querySelector(".hamBtn span");
+const navigation = document.querySelector(".navigation");
+hamBtn.addEventListener("click", () => {
+  hamBtn.classList.toggle("show");
+  navigation.classList.toggle("show");
+});
