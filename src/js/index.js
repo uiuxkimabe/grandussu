861d74a3ce919ae1b058f14fbd8214ee989343ef
uiuxkimabe <uiuxkimabe@gmail.com>
2023-08-15 22:40:48 +0700

    adding team & button booking

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,17 @@
 // scroll nav
 const nav = document.getElementsByTagName("nav")[0];
+const btnBooking = document.querySelector(".booking");
 window.addEventListener("scroll", () => {
   if (window.scrollY > 1) {
     nav.style.background = "#1d1d1d";
   } else {
     nav.style.background = "none";
+  }
+  // scroll btn booking show
+  if (window.scrollY > 485) {
+    btnBooking.classList.add("show");
+  } else {
+    btnBooking.classList.remove("show");
   }
 });
 
