4d39ccd70ad18dccad0b29fec7ec9644e9ac71d2
uiuxkimabe <uiuxkimabe@gmail.com>
2023-08-20 15:44:44 +0700

    scroll facilities

--- src/js/index.js
+++ src/js/index.js
@@ -87,10 +87,10 @@ const scrollFacilities = document.querySelector("#our-facilities .row");
 const btnPrev = document.getElementById("prev");
 const btnNext = document.getElementById("next");
 function autoScrollN() {
-  return scrollFacilities.scrollBy(266, 0);
+  return scrollFacilities.scrollBy(300, 0);
 }
 function autoScrollP() {
-  return scrollFacilities.scrollBy(-266, 0);
+  return scrollFacilities.scrollBy(-300, 0);
 }
 
 btnNext.addEventListener("click", () => {
