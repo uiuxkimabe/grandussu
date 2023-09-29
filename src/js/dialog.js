b0493d4e2d02dd4a56088760743be71be5412f1c
uiuxkimabe <uiuxkimabe@gmail.com>
2023-09-29 18:05:57 +0700

    show dialog facilities

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,15 +1,24 @@
 const facilitiesPreview = document.querySelectorAll(".img-preview");
 const dialog = document.getElementsByTagName("dialog")[0];
 const dialogContent = document.querySelectorAll(".dialog-content");
-console.info(facilitiesPreview, dialog, dialogContent);
-facilitiesPreview.forEach((imgPreview) => {
-  imgPreview.addEventListener("click", () => {
+
+function showContent(params) {
+  facilitiesPreview[params].addEventListener("click", () => {
     dialog.classList.toggle("popUp");
     dialog.style.transition = ".3s ease-in-out";
+    setTimeout(() => {
+      dialogContent[params].style.opacity = "1";
+    }, 1000);
   });
-});
+}
 
 dialog.addEventListener("click", () => {
   dialog.classList.toggle("popUp");
-  dialog.style.transition = ".3s ease-in-out";
+  location.reload();
 });
+
+showContent(0);
+showContent(1);
+showContent(2);
+showContent(3);
+showContent(4);
