4f81de51eeae3932ca924a0dcfb908d74924fbfb
uiuxkimabe <uiuxkimabe@gmail.com>
2023-10-20 22:56:41 +0700

    footer setting

--- src/js/contact.js
+++ src/js/contact.js
@@ -2,8 +2,8 @@
 const hamBtn = document.querySelector(".hamBtn span");
 const navigation = document.querySelector(".navigation");
 hamBtn.addEventListener("click", () => {
-    hamBtn.classList.toggle("show");
-    navigation.classList.toggle("show");
+  hamBtn.classList.toggle("show");
+  navigation.classList.toggle("show");
 });
 
 const custName = document.getElementById("name");
@@ -15,26 +15,26 @@ const mail = document.getElementById("mail");
 const available = document.getElementById("checkavail");
 
 const scriptURL =
-    "https://script.google.com/macros/s/AKfycbzSW45YO1XKRgprIPytZyR6i6MFvtH6tyyobXF7MhjdB12bFDx5Xn0ZvJltlzNyLZME0A/exec";
+  "https://script.google.com/macros/s/AKfycbzSW45YO1XKRgprIPytZyR6i6MFvtH6tyyobXF7MhjdB12bFDx5Xn0ZvJltlzNyLZME0A/exec";
 const form = document.forms["grand-ussu-contact-form"];
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    // ketika submit btn di klik
-    available.innerHTML = "Loading Dulu Pa Supir";
-    setTimeout(() => {
-        available.innerHTML = "Done";
-    }, 2000);
-    setTimeout(() => {
-        alert(
-            `Terima kasih untuk bantuannya ${custName.value} cantik 😘😜😜 wkwkwk \n Kalo muncul emot harusnya skrg udah masuk ke google sheetsnya`
-        );
-        form.reset();
-        available.innerHTML = "Check Available";
-    }, 2100);
+  // ketika submit btn di klik
+  available.innerHTML = "Pesan Anda Sedang di Proses";
+  setTimeout(() => {
+    available.innerHTML = "Done";
+  }, 2000);
+  setTimeout(() => {
+    alert(
+      `Terima kasih ${custName.value} telah melakukan booking di Grand Ussu Hotel & Convention. \n pesanan anda sedang kami cek, kami akan menghubungi anda segera. Mohon di tunggu.`
+    );
+    form.reset();
+    available.innerHTML = "Check Available";
+  }, 2100);
 
-    fetch(scriptURL, { method: "POST", body: new FormData(form) })
-        .then((response) => console.log("Success!", response))
-        .catch((error) => console.error("Error!", error.message));
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
 });
